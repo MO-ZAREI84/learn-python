d16bd7b7695988c9403901df7f6cbd28e01e81c4
MO-ZAREI84 <zareisanim@gmail.com>
2025-03-27 16:22:49 +0330

    entry in tkinter

--- tkinter/learn/entry.py
+++ tkinter/learn/entry.py
@@ -1,17 +1,26 @@
 from tkinter import *
-from tkinter import ttk
 
 class Root(Tk):
     def __init__(self):
         super().__init__()
-        self.title("Your App in Desktop")
+        self.title("My App")
         self.minsize(640, 320)
-        self.configure(background="#3d3d3d")
-        self.count = 0  # شمارنده دکمه
         self.init_ui()
 
     def init_ui(self):
-        pass
+        self.username = StringVar()
+
+        self.label = Label(self, text="Please enter username: ")
+        self.label.grid(column=0, row=0)
+
+        entry = Entry(self, textvariable=self.username)
+        entry.grid(column=1, row=0)
+
+        submit_btn = Button(self, text="SUBMIT", command=self.submit_username)
+        submit_btn.grid(column=2, row=0)
+
+    def submit_username(self):
+        self.label.config(text=f"your username is: {self.username.get()}")
 
 window = Root()
 window.mainloop()
